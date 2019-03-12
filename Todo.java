e5afdad1f4afbd260c92efa88bc54d74a57adc5b
Jayson Gary <jayson.t.gary@ou.edu>
2019-03-12 14:10:42 -0500

    Adding todo.java

---
+++ Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-03-12
+OU ID_______________: 113373974
+OU EMAIL____________: jayson.t.gary@ou.edu
+
+Tasks:
+1. Write the HealthyPerson Class
+2. Write the SickPerson
+3. Write PriorityQueue class
+4. Write the QueueHospital class
+5. Write StackHospital Class
+
+Estimated Time (minutes):
+1. 10
+2. 10
+3. 30
+4. 10
+5. 10
+
+Actual Time (minutes):
+1. 10
+2. 10
+3. 20
+4. 15
+5. 20
+*/}
