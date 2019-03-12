dd1c2dc8f1efe44aa77c7571c9b6bf05da859576
Jayson Gary <jayson.t.gary@ou.edu>
2019-03-12 14:59:00 -0500

    Finishing up my classes

--- src/HealthyPerson.java
+++ src/HealthyPerson.java
@@ -1,4 +1,15 @@
 
-public class HealthyPerson {
+public class HealthyPerson extends Person
+{
+	
+	public HealthyPerson(String name, int age, String reason) {
+		super(name, age);
+		
+	}
+
+	@Override
+	protected int compareToImpl(Person p) {
+		return 0;
+	}
 
 }
--- src/SickPerson.java
+++ src/SickPerson.java
@@ -1,22 +1,18 @@
 
 public class SickPerson extends Person
 {	
-	private String name;
-	private int age;
 	private int severity;
 	
 
 	public SickPerson(String name, int age, int severity) {
 		super(name, age);
-		this.name = name;
-		this.age = age;
 		this.severity = severity;
 	}
 
 
 	
 	protected int compareToImpl(Person p) {
-		// TODO Auto-generated method stub
+		
 		SickPerson sick = (SickPerson)p;
 		return sick.severity-severity;
 	}
